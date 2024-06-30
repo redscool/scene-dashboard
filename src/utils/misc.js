881f105006dadc146616fe0bef5123de37a385ff
asifkhan067 <askhan.ak912@gmail.com>
2024-06-30 17:08:31 +0530

    added server

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -21,7 +21,7 @@ export const convertTimeToHHMMFormat = (time) => {
 };
 
 export const getFileUrl = (file) => {
-  return `${config.FILE_SERVER}/uploads/${file}`;
+  return `${config.FILE_SERVER}/${file}`;
 };
 
 export const getEventCardDateFormat = (time) => {
